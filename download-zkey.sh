836e86a5fe648aac31621a7d3833ec1ef7d1c2ad
Stalanic <anustalanic@gmail.com>
2025-03-10 10:01:26 -0400

    remove duplicate & uneccesary files

--- download-zkey.sh
+++
@@ -1,5 +0,0 @@
-GIT_LFS_SKIP_SMUDGE=1 git clone https://github.com/sui-foundation/zklogin-ceremony-contributions.git
-
-cd zklogin-ceremony-contributions
-
-git lfs pull --include "zkLogin-main.zkey"
