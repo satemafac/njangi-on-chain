770f11f4595e741299caac49a5e4c0b84df9ad10
Stalanic <anustalanic@gmail.com>
2025-05-09 00:11:50 -0400

    Update favicon references to use properly scaled icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Favicon */}
-        <link rel="icon" href="/njangi-on-chain-logo.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/njangi-on-chain-logo.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/njangi-on-chain-logo.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/njangi-on-chain-logo.png" />
+        <link rel="icon" href="/icons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icons/android-chrome-512x512.png" />
         <link rel="manifest" href="/site.webmanifest" />
         
         {/* Primary Meta Tags */}
